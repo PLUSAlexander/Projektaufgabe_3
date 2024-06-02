5122ed54e03940b0bcd51af2b3b507dd9106aa7f
Alexander Neubacher <alexander.neubacher@stud.plus.ac.at>
2024-06-02 16:28:33 +0200

    klassen added

---
+++ Sofia.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+
+public class Sofia {
+
+    private static Connection con;
+    private static String url = "jdbc:postgresql://localhost/postgres";
+    private static String user = "postgres";
+    private static String pwd = "1234";
+    public static void main (String[] args) throws SQLException {
+        con = DriverManager.getConnection(url, user, pwd);
+
+
+        con.close();
+    }
+}
